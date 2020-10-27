a68db93d5cf10cb68236bdc3b079a435b7e9494c
Bobby Neal <rsneal01@gmail.com>
2020-10-27 17:33:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,13 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
- 
+    t = Time.now
     
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
  
-    if 
+    if t < 12
       resp.write "You Win"
     else
       resp.write "You Lose"
